9784882f5c90428fbfe3e7c039b8b207317fbc7f
ezekieljimlowell <88152947+ezekieljimlowell@users.noreply.github.com>
2022-05-16 15:32:21 +0530

    Changes from fallout to bocking

--- pages/index.js
+++ pages/index.js
@@ -3,23 +3,6 @@ import { MongoClient } from "mongodb";
 import Head from "next/head";
 import { Fragment } from "react";
 
-const SAMPLE_MEETUPS = [
-    {
-        id: "id1",
-        title: "first meetup",
-        image: "https://www.industrialempathy.com/img/remote/ZiClJf-640w.avif",
-        address: "No: 67, town street, tel aviv-10, israel",
-        description: "This is a first meetup"
-    },
-    {
-        id: "id2",
-        title: "second meetup",
-        image: "https://www.industrialempathy.com/img/remote/ZiClJf-640w.avif",
-        address: "No: 87, town street, tel aviv-10, israel",
-        description: "This is a second meetup"
-    }
-]
-
 function InitialPage(props) {
 
     return (
